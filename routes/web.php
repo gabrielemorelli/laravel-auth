ee0bb40ecd6869b1477ead48207f61301b2812d6
MyUsernme <gabriele.morelli@live.it>
2022-04-06 18:21:19 +0200

    esercizio

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,23 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Auth::routes();
+
+Route::middleware('auth')
+    ->namespace('Admin')
+    ->name('admin.')
+    ->prefix('admin')
+    ->group(function() {
+
+       
+        Route::get('/', 'HomeController@index')->name('home');
+
+        Route::resource('posts', 'PostController');
+
+    });
+
+    Route::get('{any?}', function() {
+        return view('guests.home');
+    })->where('any', '.*');
+    
